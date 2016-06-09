fbf737c0b0ae05a72807c32def4a7614be7b763e
shishir gowda <shishir.gowda@ril.com>
2016-06-09 13:08:15 +0000

    support parallel build
    Signed-off-by: shishir gowda <shishir.gowda@ril.com>

--- qemu_build.sh
+++ qemu_build.sh
@@ -17,6 +17,7 @@ dpkg-source -x qemu_2.0.0+dfsg-*.dsc
 cd qemu-2.0.0+dfsg
 uupdate -v 2.2.1 $1
 cd ../qemu-2.2.1
+export DEB_BUILD_OPTIONS="parallel=8"
 #echo "" >debian/patches/series
 sed -i 's/seccomp="yes"/seccomp="no"/' configure
 debian/rules build
